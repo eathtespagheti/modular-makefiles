781e9794c98460dfbe8855b7a0cb914907d66d25
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-11-04 15:51:19 +0100

    Improve node configuration

--- .makefiles/node.mk
+++ .makefiles/node.mk
@@ -1,6 +1,7 @@
-include .makefiles/docker.mk
+include .makefiles/aws.mk
 
 WEBAPP-SERVICE ?= node
+NODE-FILES-TO-CLEAN ?= $(shell find . -type d -name node_modules) $(shell find . -type f -name package-lock.json)
 
 app/node_modules: app/package.json
 	@$(DOCKER-RUN) $(WEBAPP-SERVICE) npm install
@@ -9,30 +10,35 @@ app/node_modules: app/package.json
 
 install: app/node_modules ## Install node modules
 
-start: ## Start node app
+start: generate-from-dist ## Start node app
 	@$(DOCKER-RUN) $(WEBAPP-SERVICE) npm start
 
-test: install ## Test node app
+test: install generate-from-dist ## Test node app
 	@$(DOCKER-RUN) $(WEBAPP-SERVICE) npm test
 
-test-watch: install ## Test node app and watch for changes
+test-watch: install generate-from-dist ## Test node app and watch for changes
 	@$(DOCKER-RUN) $(WEBAPP-SERVICE) npm run test-watch
 
-compile: ## Compile typescript
+compile: generate-from-dist ## Compile typescript
 	@$(DOCKER-RUN) $(WEBAPP-SERVICE) npm compile
 
-watch: ## Compile with tsc in watch mode
+watch: generate-from-dist ## Compile with tsc in watch mode
 	@$(DOCKER-RUN) $(WEBAPP-SERVICE) npm watch
 
-setup: ## Setup environment from scratch
+setup: generate-from-dist ## Setup environment from scratch
 	@$(MAKE) build
 	@$(MAKE) install
 
-debug: ## Start node app but listen and wait for debugger
+debug: generate-from-dist ## Start node app but listen and wait for debugger
 	@-$(DOCKER-RUN) -d -p 9229:9229 $(WEBAPP-SERVICE) npm run debug
+
+debug-tests: generate-from-dist ## Start tests but listen and wait for debugger
+	@-$(DOCKER-RUN) -d -p 9229:9229 $(WEBAPP-SERVICE) npm run debug-tests
 
 debug-stop: ## Stop all debugging containers
 	@$(MAKE) down-$(WEBAPP-SERVICE)
 
+nuke-node:
+	@-rm -rf $(NODE-FILES-TO-CLEAN)
 
-.PHONY: install start test compile watch setup test-watch debug debug-stop
+.PHONY: install start test compile watch setup test-watch debug debug-stop nuke-node
