c4e9ba680a8d22eaf29afd0c6acb5b6fa41d469c
Fabio Sussarellu <sussarellu.fabio@gmail.com>
2021-11-30 12:36:45 +0100

    Fix wrong git commands

--- .makefiles/git.mk
+++ .makefiles/git.mk
@@ -6,8 +6,8 @@ nuke-git: ## Remove all files in gitignore and reset the git repo
 
 .PHONY: update-submodules
 update-submodules: ## Update all submodules
-	@git submodule update --recursive --remote
+	@git pull --recurse-submodules --jobs=10
 
 .PHONY: init-submodules
 init-submodules: ## Initialize all submodules
-	@git pull --recurse-submodules --jobs=10
+	@git submodule update --init --recursive
